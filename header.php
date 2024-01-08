c98fd0e8c0a0def6459b3259b775096e16aed2e9
Bekoo023 <162413@novacollege.nl>
2024-01-08 21:36:43 +0100

    Eerste

---
+++ header.php
@@ -0,0 +1,7 @@
+<div class="bovenheader">
+    <p>hallo</p>
+</div>
+
+<div class="header">
+    <p class="hoofdtext">Work4Me</p>
+</div>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <script src="script.js"></script>
+    <title>Work4Me</title>
+</head>
+<body>
+    <?php include 'header.php' ?>
+
+    <main>
+
+    </main>
+
+    <?php include 'footer.php' ?>
+
+</body>
+</html>
